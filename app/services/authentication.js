7b4eda22d5f1f7504cd43a30e12c5cfed0262d24
j.u.p.iter <pn79122821224@gmail.com>
2019-04-14 14:41:17 +0500

    creates authentication service creator template

---
+++ app/services/authentication.js
@@ -0,0 +1,24 @@
+const createAuthenticationService = () => {
+  const hashPassword = () => {
+
+  };
+
+  const comparePasswords = () => {
+
+  };
+
+  const generateToken = () => {
+
+  };
+
+  const decodeToken = () => {
+
+  }
+
+  return {
+    hashPassword,
+    comparePasswords,
+    generateToken,
+    decodeToken,
+  };
+};
