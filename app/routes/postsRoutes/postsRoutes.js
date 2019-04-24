0ab6ff96becb4644a364bf279596dbb7797e99a6
j.u.p.iter <pn79122821224@gmail.com>
2019-04-24 19:02:02 +0500

    adds posts routes

--- app/routes/postsRoutes/postsRoutes.js
+++ app/routes/postsRoutes/postsRoutes.js
@@ -1,9 +1,22 @@
 export const createPostsRoutes = ({
   router,
+  postController: {
+    create,
+    readAll,
+    readOne,
+    update,
+    deleteOne,
+  },
 }) => {
-  router.get('/', (req, res) => {
-    res.body = 'Hello from posts!';
-  });
+  router.post('/', create);
+
+  router.get('/', readAll);
+
+  router.get('/:postId', readOne);
+
+  router.put('/:postId', update);
+
+  router.delete('/:postId', deleteOne);
 
   return router;
 };
