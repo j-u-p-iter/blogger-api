d58bd91fc72ece693ccd5d1e39eadc2a20a488b5
j.u.p.iter <pn79122821224@gmail.com>
2019-04-24 19:48:35 +0500

    renames BASE PART to BASE_PATH

--- app/urls/usersUrls/usersUrls.js
+++ app/urls/usersUrls/usersUrls.js
@@ -4,8 +4,8 @@ const createPath = path => `${BASE_PATH}/${path}`;
 
 
 export const createUsersUrls = ({ utils: { makeApiUrl } }) => ({
-  get: () => makeApiUrl(BASE_PART),
-  post: () => makeApiUrl(BASE_PART),
+  get: () => makeApiUrl(BASE_PATH),
+  post: () => makeApiUrl(BASE_PATH),
   getOne: id => makeApiUrl(createPath(id)),
   patch: id => makeApiUrl(createPath(id)),
   delete: id => makeApiUrl(createPath(id)),
