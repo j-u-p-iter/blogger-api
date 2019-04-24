41a916e2011c8751c74dae25ae5ab992ffecc6d8
j.u.p.iter <pn79122821224@gmail.com>
2019-04-24 19:40:59 +0500

    adds postsUrls

---
+++ app/urls/postsUrls/postsUrls.js
@@ -0,0 +1,12 @@
+const BASE_PATH = 'posts';
+
+const createPath = path => `${BASE_PATH}/${path}`;
+
+
+export const createPostsUrls = ({ utils: { makeApiUrl } }) => ({
+  get: () => makeApiUrl(BASE_PATH),
+  post: () => makeApiUrl(BASE_PATH),
+  getOne: id => makeApiUrl(createPath(id)),
+  patch: id => makeApiUrl(createPath(id)),
+  delete: id => makeApiUrl(createPath(id)),
+});
