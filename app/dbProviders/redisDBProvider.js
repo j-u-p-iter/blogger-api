f77a3c3032504a4e03e54a3f93b4bc06da89f43c
j.u.p.iter <pn79122821224@gmail.com>
2019-05-05 20:13:38 +0500

    reads redis configs

--- app/dbProviders/redisDBProvider.js
+++ app/dbProviders/redisDBProvider.js
@@ -3,11 +3,19 @@ import bluebird from 'bluebird';
 
 const promisifiedRedis = bluebird.promisifyAll(redis); 
 
-export const createRedisDBProvider = () => {
+export const createRedisDBProvider = ({
+  configs: {
+    REDIS_PORT,
+    REDIS_HOST,
+  },
+}) => {
   let client;
 
   const create = async () => {
-    client = await redis.createClient();
+    client = await redis.createClient({
+      host: REDIS_HOST,
+      port: REDIS_PORT,
+    });
 
     return client;
   } 
